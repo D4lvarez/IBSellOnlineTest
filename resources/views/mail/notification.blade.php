d60b8e06fe1cc68da811fe37e0bc2deb4f82aff7
Diego Alvarez <andresdaap2015@gmail.com>
2021-04-29 17:56:22 -0400

    Email template

---
+++ resources/views/mail/notification.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test interview by Diego Alvarez</title>
+</head>
+
+<body>
+    We have a new registration.
+    User: {{ $user }}
+    Email: {{ $email }}
+</body>
+
+</html>
