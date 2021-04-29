d44b753f393a4d1c49a126eecfd9949f0572bc8a
Diego Alvarez <andresdaap2015@gmail.com>
2021-04-29 09:24:42 -0400

    RegisterController form

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NewUser;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function main(Request $request)
+    {
+
+        return view('forms.register', [
+            'title' => 'Register Form'
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $username = $request->username;
+        $email = $request->email;
+        $image = $request->imageUser;
+
+        $path = $image->store('media');
+
+        $new_user = NewUser::create([
+            'username' => $username,
+            'email' => $email,
+            'image' => $path
+        ]);
+    }
+
+    public function read(Request $request)
+    {
+        return response()->json(NewUser::all());
+    }
+}
