684216b30e325fe2712ca61f9dfcca986a384e28
Diego Alvarez <andresdaap2015@gmail.com>
2021-04-29 17:55:17 -0400

    Remove store and read functions

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -7,33 +7,12 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    public function main(Request $request)
+    public function main()
     {
-
+        $users = NewUser::all();
         return view('forms.register', [
-            'title' => 'Register Form'
+            'title' => 'Register Form',
+            'users' => $users,
         ]);
-    }
-
-    public function store(Request $request)
-    {
-        $username = $request->username;
-        $email = $request->email;
-        $image = $request->imageUser;
-        $phone = $request->phone;
-
-        $path = $image->store('media');
-
-        $new_user = NewUser::create([
-            'username' => $username,
-            'email' => $email,
-            'image' => $path,
-            'phone' => $phone,
-        ]);
-    }
-
-    public function read(Request $request)
-    {
-        return response()->json(NewUser::all());
     }
 }
