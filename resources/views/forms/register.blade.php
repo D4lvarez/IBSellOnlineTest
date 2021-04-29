69719317713f2eb9a4cbaa5eab79d30615b7d6f8
Diego Alvarez <andresdaap2015@gmail.com>
2021-04-29 17:53:27 -0400

    Added Livewire's Components

--- resources/views/forms/register.blade.php
+++ resources/views/forms/register.blade.php
@@ -9,67 +9,31 @@
                 {{ $title }}
             </div>
             <div class="card-body">
-                <form>
-                    @csrf
-                    {{-- Username Field --}}
-                    <div class="row g-3 align-items-center">
-                        <div class="col-2">
-                            <label for="username" class="col-form-label">Username</label>
-                        </div>
-                        <div class="col-2">
-                            <input type="text" id="username" name="username" required class="form-control">
-                        </div>
-                    </div>
-                    {{-- Email Field --}}
-                    <div class="row g-3 align-items-center mt-3">
-                        <div class="col-2">
-                            <label for="email" class="col-form-label">Email</label>
-                        </div>
-                        <div class="col-2">
-                            <input type="email" id="email" name="email" required class="form-control">
-                        </div>
-                    </div>
-                    {{-- Phone Field --}}
-                    <div class="row g-3 align-items-center mt-3">
-                        <div class="col-2">
-                            <label for="phone" class="col-form-label">Phone</label>
-                        </div>
-                        <div class="col-2">
-                            <input type="number" id="phone" name="phone" required class="form-control">
-                        </div>
-                    </div>
-                    {{-- Image Field --}}
-                    <div class="row g-3 align-items-center mt-3">
-                        <div class="col-auto" style="margin-left: 59px">
-                            <label class="input-group-text" for="imageUser">Upload Image</label>
-                            <input type="file" class="form-control" required name="imageUser" id="imageUser">
-                        </div>
-                    </div>
-                    <button type="submit" class="btn btn-primary mt-2">Submit</button>
-                </form>
+                @livewire('register-form')
             </div>
         </div>
 
-        {{-- Table Show --}}
-        <table class="table table-striped mt-5">
 
+    </div>
+
+    {{-- Table Show --}}
+    <div class="container">
+        <table class="table table-striped mt-5">
             <thead>
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">Username</th>
                     <th scope="col">Email</th>
                     <th scope="col">Phone</th>
+                    <th scope="col">Image</th>
                 </tr>
             </thead>
-            <tbody id="table-body">
-
-            </tbody>
-
+            @livewire('table-row')
         </table>
     </div>
 @endsection
 
 @section('own_js')
-    <script src="js/app.js"></script>
+    {{-- <script src="js/app.js"></script> --}}
 
 @endsection
